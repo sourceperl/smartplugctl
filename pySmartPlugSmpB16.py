5f76588e5169d5f4aa5a59117e5511b127eaeb8d
Nicolas Graziano <nicolas.graziano@gmail.com>
2017-01-12 17:11:53 +0100

    Add generic way to generate message and receive message.
    Generate header, length, checksum for message.
    Add generic way to handle notify, prepare for long message.

--- pySmartPlugSmpB16.py
+++ pySmartPlugSmpB16.py
@@ -1,6 +1,8 @@
 import binascii
 from bluepy import btle
 
+START_OF_MESSAGE = bytes([0x0f])
+END_OF_MESSAGE = bytes([0xff, 0xff])
 
 class SmartPlug(btle.Peripheral):
     def __init__(self, addr):
@@ -12,21 +14,31 @@ class SmartPlug(btle.Peripheral):
 
     def on(self):
         self.delegate.chg_is_ok = False
-        self.plug_cmd_ch.write(binascii.unhexlify('0f06030001000005ffff'))
-        self.waitForNotifications(0.5)
+        self.plug_cmd_ch.write(self.get_buffer(binascii.unhexlify('0300010000')))
+        self.wait_data(0.5)
         return self.delegate.chg_is_ok
 
     def off(self):
         self.delegate.chg_is_ok = False
-        self.plug_cmd_ch.write(binascii.unhexlify('0f06030000000004ffff'))
-        self.waitForNotifications(0.5)
+        self.plug_cmd_ch.write(self.get_buffer(binascii.unhexlify('0300000000')))
+        self.wait_data(0.5)
         return self.delegate.chg_is_ok
 
     def status_request(self):
-        self.plug_cmd_ch.write(binascii.unhexlify('0f050400000005ffff'))
-        self.waitForNotifications(2.0)
+        self.plug_cmd_ch.write(self.get_buffer(binascii.unhexlify('04000000')))
+        self.wait_data(2.0)
         return self.delegate.state, self.delegate.power
 
+    def calculate_checksum(self, message):
+        return ((sum(message) + 1) & 0xff).to_bytes(1, byteorder='big')
+
+    def get_buffer(self, message):
+        return START_OF_MESSAGE + (len(message) + 1).to_bytes(1,byteorder='big') + message +  self.calculate_checksum(message) + END_OF_MESSAGE 
+
+    def wait_data(self, timeout):
+        self.delegate.need_data = True
+        while self.delegate.need_data and self.waitForNotifications(timeout):
+            pass
 
 class NotificationDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -34,16 +46,32 @@ class NotificationDelegate(btle.DefaultDelegate):
         self.state = False
         self.power = 0
         self.chg_is_ok = False
+        self._buffer = b''
+        self.need_data = True
 
     def handleNotification(self, cHandle, data):
-        bytes_data = bytearray(data)
+        #not sure 0x0f indicate begin of buffer but
+        if data[:1] == START_OF_MESSAGE:
+            self._buffer = data
+        else:
+            self._buffer = self._buffer + data
+        if self._buffer[-2:] == END_OF_MESSAGE:
+            self.handle_data(self._buffer)
+            self._buffer = b''
+            self.need_data = False 
+
+    def handle_data(self, bytes_data):
         # it's a state change confirm notification ?
-        if bytes_data[0:3] == bytearray([0x0f, 0x04, 0x03]):
+        if bytes_data[0:3] == bytes([0x0f, 0x04, 0x03]):
             self.chg_is_ok = True
         # it's a state/power notification ?
-        if bytes_data[0:3] == bytearray([0x0f, 0x0f, 0x04]):
+        if bytes_data[0:3] == bytes([0x0f, 0x0f, 0x04]):
             self.state = bytes_data[4] == 1
             self.power = int(binascii.hexlify(bytes_data[6:10]), 16) / 1000
+        # it's a 0x0a notif ?
+        if bytes_data[0:3] == bytes([0x0f, 0x33, 0x0a]):
+            print ("0A notif %s" % bytes_data)
+
 
 
 # SmartPlugSmpB16 usage sample: cycle power then log plug state and power level to terminal
