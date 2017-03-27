7acb6eaf8e5cfeda7b685d226f1da9c892834e95
sourceperl <loic.celine@free.fr>
2017-03-27 10:38:47 +0200

    set version to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='smartplugctl',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     url='https://github.com/sourceperl/smartplugctl',
     platforms='any',
